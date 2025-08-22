91945c8499e190c7b04004d6e385c1e6997a2497
dududavid <gazit.david@gmail.com>
2025-08-22 10:27:47 +0300

    list

--- ToDoList.js
+++ ToDoList.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const port = 3000
+const port = 3000;
+let list = [];
 app.use(express.json());
 app.get('/', (req, res) =>
 {res.sendFile(__dirname + '/ToDoList.html')});
 app.listen(port, () => {console.log(`http://localhost:${port}`)});
+app.get('/list', (req, res) => {
+    res.json(list);
+})
